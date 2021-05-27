ccd08201129be71ffc3ea6566d650e0443581fe4
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-05-27 12:35:07 +0800

    Create run_gemini_GENE.sh

---
+++ run_gemini_GENE.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DATABASE=wes_hg38.database
+GENE=$1
+#Example usage ./run_gemini_GENE.sh ACTA1 > ACTA1.txt
+gemini query -q \
+        "select gene, qual, chrom, start, end, ref, alt, codon_change, aa_change, aa_length, impact, impact_severity, transcript, HGVSc, HGVSp, \
+        exon,\
+        intron, \
+        type, \
+        cDNA_position, \
+        CADD_PHRED, \
+        polyphen_pred, \
+        sift_pred, \
+        Loftool, \
+        SpliceAI_pred_DP_AG, \
+        SpliceAI_pred_DP_AL, \
+        SpliceAI_pred_DP_DG, \
+        SpliceAI_pred_DP_DL, \
+        SpliceAI_pred_DS_AG, \
+        SpliceAI_pred_DS_AL, \
+        SpliceAI_pred_DS_DG, \
+        SpliceAI_pred_DS_DL, \
+        SpliceAI_pred_SYMBOL, \
+        gnomADg, \
+        gnomADg_AF, \
+        gnomADg_AN, \
+        gnomADg_AC, \
+        gnomADg_AF_popmax,  gnomAD_AFR_AF, gnomAD_AMR_AF, gnomAD_ASJ_AF, gnomAD_EAS_AF, gnomAD_FIN_AF, gnomAD_NFE_AF, gnomAD_OTH_AF, gnomAD_SAS_AF, \
+        gnomADg_nhomalt, \
+        ensembl_gene_id, \
+        transcript, \
+        is_exonic, \
+        is_coding, \
+        is_lof, \
+        is_splicing, \
+        is_canonical, \
+        biotype, \
+        ensp, \
+        swissprot, \
+        domains, \
+        UNIPARC, \
+        UNIPROT_ISOFORM, \
+        GENE_PHENO, \
+        PHENO, \
+        CLIN_SIG, \
+        Clinvar \
+        from variants where qual >=100 and (MAX_AF_POPS >= 0.1) and (gnomADg_AF_popmax >= 0.1) and gene='$GENE'" \
+    --sample-delim ";" \
+    --show-samples \
+    --header \
+   $DATABASE
